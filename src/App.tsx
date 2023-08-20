9f90d87f9ea439c58b98d34903aa45bb9b21b8af
joaopelisson <joaopedro.devbr@gmail.com>
2023-08-20 10:55:02 -0300

    feat: initial structure and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button />
-      <Button variant="success"/>
     </ThemeProvider>
   )
 }

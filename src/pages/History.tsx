b6506f7603654a1a7d77653006a1db6e977dcb81
joaopelisson <joaopedro.devbr@gmail.com>
2023-08-23 07:19:11 -0300

    feat: Page 'History'

--- src/pages/History.tsx
+++
@@ -1,7 +0,0 @@
-export function History(){
-    return (
-        <>
-            History
-        </>
-    )
-}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,9 +7,21 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput placeholder="Dê um nome para sua tarefa" type="text" id="task" />
+                    <TaskInput list="task-suggestions" placeholder="Dê um nome para sua tarefa" type="text" id="task" />
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 01" />
+                        <option value="Projeto 02" />
+                        <option value="Projeto 03" />
+                    </datalist>
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput placeholder="00" type="number" id="minutesAmount" />
+                    <MinutesAmountInput
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                        type="number"
+                        id="minutesAmount"
+                    />
                     <span>minutos.</span>
                 </FormContainer>
                 <CountdownContainer>
@@ -19,7 +31,7 @@ export function Home() {
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
-                <StartCountdownButton  disabled type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
